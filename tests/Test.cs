423890e2ba7f74b18b0bcf18254375734c5bacb4
Terry Matthew Poulin <BigBoss1964@gmail.com>
2010-07-31 02:55:44 +0000

    Commit tests for code in commit acbc885e242cfabc2f7ac0f157cb35bbc387f96b
    These need to be cleaned up, rather than made repetitive. Either using
    delegates or generics should be fine. Making it run the tests
    concurrently in different threads is probably a good idea too.

--- tests/Test.cs
+++ tests/Test.cs
@@ -1,17 +1,67 @@
 using System;
 using VcsSharp;
 
+///
+/// Tests that are run:
+///     factory function for opening existing repos
+///
+///     default constructor and Init() works.
+///         both with and without an existing repo
+///
 class Test {
 
     public static void Main(string[] args) {
         Console.WriteLine(
                 "Testing git... " + (Git() ? "pass" : "fail")
         );
+        Console.WriteLine(
+                "Testing mercurial... " + (Hg() ? "pass" : "fail")
+        );
+        Console.WriteLine(
+                "Testing bazaar... " + (Bzr() ? "pass" : "fail")
+        );
     }
 
     static bool Git() {
 
+        Vcs git = new Git();
+        if (!git.Init("/tmp/test.git")) {
+            Console.WriteLine("Git.Init() failed");
+            return false;
+        }
+
         Vcs vcs = Factory.GetRepo("/tmp/test.git");
+        if (vcs == null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    static bool Hg() {
+
+        Vcs hg = new Hg();
+        if (!hg.Init("/tmp/test.hg")) {
+            Console.WriteLine("Hg.Init() failed");
+            return false;
+        }
+
+        Vcs vcs = Factory.GetRepo("/tmp/test.hg");
+        if (vcs == null) {
+            return false;
+        }
+
+        return true;
+    }
+    static bool Bzr() {
+
+        Vcs bzr = new Bzr();
+        if (!bzr.Init("/tmp/test.bzr")) {
+            Console.WriteLine("Bzr.Init() failed");
+            return false;
+        }
+
+        Vcs vcs = Factory.GetRepo("/tmp/test.bzr");
         if (vcs == null) {
             return false;
         }
